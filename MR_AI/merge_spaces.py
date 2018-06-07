e08733f43a2e5c7efe4163e5949474b0924d3de7
LiTigre <li.zhang17@mail.mcgill.ca>
2018-06-07 16:21:14 -0400

    added gaussian blur of 0.1 in the merge function

--- MR_AI/merge_spaces.py
+++ MR_AI/merge_spaces.py
@@ -4,14 +4,8 @@ import cv2
 from PIL import Image
 import nibabel as nib
 from matplotlib import pyplot as plt
+from scipy.ndimage import gaussian_filter
 
-# #choose image that you want to convert to kspace
-# T1 = '/Users/li-tigre/Downloads/og.jpg'
-# T2 = '/Users/li-tigre/Downloads/tilted.jpg'
-
-# #read the image
-# img_1 = cv2.imread(T1, 0)
-# img_2 = cv2.imread(T2, 0)
 
 
 def merge_k_spaces(og_image, tilted_image, percentage = 0.5):
@@ -20,14 +14,34 @@ def merge_k_spaces(og_image, tilted_image, percentage = 0.5):
 	f_1 = np.fft.fft2(img_1)
 	f_2 = np.fft.fft2(img_2)
 
-	#add the two images together
+	#add the two images together with the percentage
 	f_out = f_1*(1-percentage) + f_2*percentage
 
+	#reverse the fft
+	arr_out = np.fft.ifft2(f_out)
 
-	rip_1 = np.fft.ifft2(f_out)
+	img_out = Image.fromarray(np.abs(arr_out))
+	img_blur = gaussian_filter(img_out, sigma=0.1)
 
-	rip2 = Image.fromarray(np.abs(rip_1))
-	rip2.show()
+	#temporary to show the image
+	print(type(img_blur))
+	img_show = Image.fromarray(np.abs(img_blur))	
+	print(type(img_show))
+	img_show.show()
 
 
-	return rip_1
+	return img_blur
+
+
+#choose image that you want to convert to kspace
+T1 = '/Users/li-tigre/Downloads/og.jpg'
+T2 = '/Users/li-tigre/Downloads/tilted_1.jpg'
+
+#read the image
+img_1 = cv2.imread(T1, 0)
+img_2 = cv2.imread(T2, 0)
+
+merge_k_spaces(img_1, img_2)
+
+
+
